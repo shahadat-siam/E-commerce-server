cd3fa95bf80fb33544b3a6a933a1acdb54034442
shahadat-siam <lonelinessismycompanion09@gmail.com>
2024-09-27 17:18:58 +0600

    post a user & product data in db and also get user role

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-// ----- middlewere ------
-const corsOption ={
-    origin: ["http://localhost:5173","http://localhost:5174"],
-    credentials: true,
-    optionSuccessStatus: 200,
-}
+// ----- middlewere ------ 
+const corsOption = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOption));
 app.use(express.json());
 
@@ -30,11 +30,47 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-     
-    
+
+    const userCollection = client.db("MSshop").collection("user"); 
+    const productCollection = client.db("MSshop").collection("product"); 
+ 
+    // save user data and role in db
+    app.put("/user", async (req, res) => {
+        const user = req.body 
+        const query = {email: user?.email}
+        // check user already exist
+        const isExist = await userCollection.findOne(query) 
+        if(isExist){
+            return res.send(isExist)
+        }
+        const option = {upsert: true}
+        const updateDoc = {
+            $set: {
+                ...user,
+                timestams: Date.now()
+            }
+        }
+        const result = await userCollection.updateOne(query, updateDoc, option)
+        res.send(result)
+    })
+
+    // check role jeneral user or admin
+    app.get("/role-call/:email", async (req, res) => {
+      const email = req.params.email;
+      // console.log(email)
+      const result = await userCollection.findOne({email});
+      res.send(result);
+    });
+
+    app.post("/product-add", async(req, res) => {
+      const productData = req.body
+      const result = await productCollection.insertOne(productData)
+      res.send(result) 
+    })
+      
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally { 
-    // await client.close();
+  } finally {  
+
   }
 }
 run().catch(console.dir);
